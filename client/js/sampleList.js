cf9ef0eed88325fbb1e5a6139d828aaf34e7b21d
Bill Pierce <bill.pierce@negawatt.com>
2015-01-01 21:35:25 -0500

    display table of wave files

--- client/js/sampleList.js
+++ client/js/sampleList.js
@@ -1,27 +1,41 @@
-var wavesurfer = Object.create(WaveSurfer);
-
-wavesurfer.init({
-    container: document.querySelector('#wave'),
-    waveColor: 'violet',
-    progressColor: 'purple'
-});
-
 function playWaveFile()
 {
     wavesurfer.play();
 }
 
-wavesurfer.load('/data/wav_files/Side_Left.wav');
+function loadWave(container, wave_path)
+{
+    var wavesurfer = Object.create(WaveSurfer);
+
+    wavesurfer.init({
+        //container: document.querySelector(container_id),
+        container: container,
+        waveColor: 'violet',
+        progressColor: 'purple',
+        cursorWidth: 0
+    });
+    
+    wavesurfer.load(wave_path);
+}
 
 function mainLoop()
 {
-    // Retrieve list of sample filenames.
-    //$("#waveList").load('/sample_list');
+    var waveList = $("#waveList");
+    var waveTable = $("<table style='width:100%'/>", { "border" : 1 }).appendTo(waveList);
 
+    // Retrieve list of sample filenames.
     $.get("/sample_list", function(sample_list,status){
-        //$("#waveList").load(sample_list);
         for(var i = 0; i < sample_list.length; i++) {
-            $("#waveList").append(sample_list[i]);
+            var sampleName = sample_list[i];
+
+            // For each filename, create a table entry and load a WaveSurfer object
+            var row = $("<tr>").appendTo(waveTable);
+            var col1 = $("<td style='width:10%'>").text(sampleName).appendTo(row);
+            var sampleId = "wave" + i;
+            var col2 = $("<td>").attr("id", sampleId).appendTo(row);
+
+            console.log(sampleId);
+            loadWave(col2[0], '/data/wav_files/Side_Left.wav');
         }
     });
 }
